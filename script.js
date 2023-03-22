91c710d3054d1275cb3343f02ae6f7202ef8d908
Thomas Anderson <andrzej.krzysztof.hryniewicz@gmail.com>
2023-03-22 15:46:48 +0200

    xxx

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 500;
-const CANVAS_HEIGHT = canvas.height = 1000;
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 1000;
 
-enemy = {
-    x: 0,
-    y: 0,
-    width: 200,
-    height: 200
-}
+
+
+class Enemy {
+    constructor(){
+        this.x = 10;
+        this.y = 50;
+        this.width = 100;
+        this.height = 100;
+    }
+    
+};
+const enemy1 = Enemy();
 
 function animate(){
     ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -19,4 +25,3 @@ function animate(){
     requestAnimationFrame(animate);
 }
 animate();
-
