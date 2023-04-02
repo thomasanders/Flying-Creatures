25075145f39008dd86ca1923f8fd68f80e140bf6
Thomas Anderson <andrzej.krzysztof.hryniewicz@gmail.com>
2023-04-02 19:58:52 +0300

    working code

--- script.js
+++ script.js
@@ -3,25 +3,50 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 20;
+const enemiesArray = [];
 
-
+let gameFrame = 0;
 
 class Enemy {
     constructor(){
-        this.x = 10;
-        this.y = 50;
-        this.width = 100;
-        this.height = 100;
+        this.Image = new Image();
+        this.Image.src = 'enemy1.png'
+        //this.speed = Math.random() * 4 - 2;
+        this.spriteWidth = 293;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth / 2.5; 
+        this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height); 
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1); // waving wings differnetly
     }
-    
+    update(){
+        this.x += Math.random() * 5 - 2.5;
+        this.y += Math.random() * 5 - 2.5;  
+      
+        // animate sprites
+        if (gameFrame % this.flapSpeed === 0){
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+    draw(){
+        ctx.drawImage(this.Image, this.frame * this.spriteWidth,  0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }    
 };
-const enemy1 = Enemy();
+
+for (let i = 0; i < numberOfEnemies; i ++){
+    enemiesArray.push(new Enemy());
+}
 
 function animate(){
-    ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    enemy1.x++;
-    enemy1.y++;
-    ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw();
+    });
+    gameFrame++;
     requestAnimationFrame(animate);
 }
 animate();
